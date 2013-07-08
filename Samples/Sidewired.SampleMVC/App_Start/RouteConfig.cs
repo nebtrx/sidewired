a6a4be96f9e998decc476347db7f65fd55396024
Omar Ahmed <neb.trx@gmail.com>
2013-07-07 22:25:58 -0400

    Nuget Packaging

--- Samples/Sidewired.SampleMVC/App_Start/RouteConfig.cs
+++ Samples/Sidewired.SampleMVC/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace Sidewired.SampleMVC.App_Start
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Smooth", id = UrlParameter.Optional }
             );
         }
     }
